b9a44d7655f6d5066dd946f30838090e0edd93df
IagoLast <iago.lastra@gmail.com>
2017-03-23 18:07:57 +0100

    Add list filtering example
    This shows the equivalent to the ng-repeat directive with wc

---
+++ components/list-filtering.component.js
@@ -0,0 +1,39 @@
+class ListFiltering extends WC {
+  get initialState() {
+    return {
+      query: '',
+    }
+
+  }
+
+  get template() {
+    return `
+      <h1>List filtering</h1>
+      <input></input>
+      <ul></ul>
+    `;
+  }
+
+  get languages() {
+    return ['c', 'c++', 'java', 'javascript', 'bash'];
+  }
+
+  connectedCallback() {
+    this.content = this.template; // Initial render
+    this.content.querySelector('input').addEventListener('input', this.onInput.bind(this));
+  }
+
+  onInput(event) {
+    this.setState({
+      query: event.target.value,
+    })
+  }
+
+  render() {
+    let filtered = this.languages.filter(item => item.includes(this.state.query.toLowerCase()));
+    console.log(filtered);
+    this.content.querySelector('ul').innerHTML = filtered.map(item => `<li>${item}</li>`).join('');
+  }
+}
+
+customElements.define('list-filtering', ListFiltering);
